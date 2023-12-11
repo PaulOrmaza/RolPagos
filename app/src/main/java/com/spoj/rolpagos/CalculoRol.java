a2dbb535f2b9def747b44d48b1370c4f2fe8f666
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2023-12-10 19:25:34 -0500

    Creacion de pantalla de ingreso de datos

---
+++ app/src/main/java/com/spoj/rolpagos/CalculoRol.java
@@ -0,0 +1,14 @@
+package com.spoj.rolpagos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculoRol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo_rol);
+    }
+}
